1e0de8df1a509a6a4b870a40551da32a61653635
scienide00 <dimitrovite@yahoo.com>
2014-01-14 00:35:29 +0200

    Moved some classes from/to the Framework folder for better organization.

---
+++ MapEditor/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        var sReader = new StreamReader(@"..\..\maps\testmap.wocm", System.Text.Encoding.ASCII);
+
+    }
+}
---
+++ z FlagBinaryDecimalConversion/FlagBinaryDecimalConversion.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace FlagConverter
+{
+    public static class FlagBinaryDecimalConversion
+    {
+        public static void ShowBinary(int flag)
+        {
+            string binaryFlags = Convert.ToString(flag, 2);
+            Console.WriteLine("Flag {0}: {1}\n", flag, binaryFlags);
+            for (int i = 0; i < binaryFlags.Length; i++)
+            {
+                Console.Write("{0}", i);
+            }
+
+            Console.WriteLine();
+            for (int k = binaryFlags.Length - 1; k >= 0; k--)
+            {
+                Console.Write("{0}", binaryFlags[k]);
+            }
+            Console.WriteLine();
+            ShowActiveFlags(binaryFlags);
+        }
+
+        public static void ShowDecimal(string flags)
+        {
+            int decFlag = Convert.ToInt32(flags, 2);
+            Console.WriteLine("Flag {0}: {1}\n", flags, decFlag);
+        }
+
+        public static void ShowActiveFlags(string binaryFlags)
+        {
+            Console.WriteLine("\nActive Flags (==true):");
+
+            string str = new string('0', 32 - binaryFlags.Length);
+            binaryFlags = str + binaryFlags;
+
+            if (binaryFlags[binaryFlags.Length - 1] == '1')
+            {
+                Console.WriteLine("Is Movable");
+            }
+            if (binaryFlags[binaryFlags.Length - 2] == '1')
+            {
+                Console.WriteLine("Is Collidable");
+            }
+            if (binaryFlags[binaryFlags.Length - 3] == '1')
+            {
+                Console.WriteLine("Has Effect");
+            }
+            if (binaryFlags[binaryFlags.Length - 4] == '1')
+            {                
+                Console.WriteLine("Is Transparent");
+            }
+            Console.WriteLine("\n");
+        }
+
+        public static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.CursorVisible = false;
+            Console.WriteLine("BINARY -> DECIMAL [b]");
+            Console.WriteLine("DECIMAL -> BINARY [d]\n\n");
+
+            bool loop = true;
+            while (loop)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.B:
+                        Console.Write("Input the binary flag: ");
+                        ShowDecimal(Console.ReadLine());
+                        break;
+
+                    case ConsoleKey.D:
+                        Console.Write("Input the decimal flag: ");
+                        ShowBinary(int.Parse(Console.ReadLine()));
+                        break;
+
+                    case ConsoleKey.Escape:
+                        loop = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
